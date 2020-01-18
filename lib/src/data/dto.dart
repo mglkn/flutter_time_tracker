14280357d4157b0e9da2f40379e4425a23e4f6c6
Vladimir <iammigalkin@yandex.com>
2020-01-18 15:07:13 +0300

    add dto;

--- lib/src/data/dto.dart
+++ lib/src/data/dto.dart
@@ -4,22 +4,22 @@ import 'db.dart';
 
 class GoalWithTagsAndPomodorosCount {
   final Goal goal;
-  // final List<Tag> tags;
-  // final int pomodorosCount;
+  final List<Tag> tags;
+  final int pomodorosCount;
 
   GoalWithTagsAndPomodorosCount({
     @required this.goal,
-    // @required this.tags,
-    // @required this.pomodoros,
+    @required this.tags,
+    @required this.pomodorosCount,
   });
 }
 
-// class TagWithPomodorosCount {
-//   final Tag tag;
-//   final int pomodorosCount;
+class TagWithPomodorosCount {
+  final Tag tag;
+  final int pomodorosCount;
 
-//   TagWithPomodorosCount({
-//     @required this.tag,
-//     @required this.pomodorosCount,
-//   });
-// }
+  TagWithPomodorosCount({
+    @required this.tag,
+    @required this.pomodorosCount,
+  });
+}
