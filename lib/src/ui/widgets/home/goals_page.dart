3f7075a660c31eddfde627115cc457940f545a75
Vladimir <iammigalkin@yandex.com>
2020-01-22 10:27:47 +0300

    add tags to GoalListTile;

--- lib/src/ui/widgets/home/goals_page.dart
+++ lib/src/ui/widgets/home/goals_page.dart
@@ -12,18 +12,29 @@ class _GoalsPageState extends State<GoalsPage> {
   final _listTestGoals = [
     GoalWithTagsAndPomodorosCount(
       goal: Goal(label: 'first goal'),
-      tags: [],
-      pomodorosCount: 12,
+      tags: [
+        Tag(label: 'programming', color: Colors.purple.value),
+        Tag(label: 'dart', color: Colors.blue.value),
+        Tag(label: 'flutter', color: Colors.orange.value),
+      ],
+      pomodorosCount: 11,
     ),
     GoalWithTagsAndPomodorosCount(
       goal: Goal(label: 'second goal'),
-      tags: [],
-      pomodorosCount: 12,
+      tags: [
+        Tag(label: 'programming', color: Colors.purple.value),
+        Tag(label: 'algorithms', color: Colors.teal.value),
+        Tag(label: 'swift', color: Colors.red.value),
+      ],
+      pomodorosCount: 122,
     ),
     GoalWithTagsAndPomodorosCount(
       goal: Goal(label: 'third goal'),
-      tags: [],
-      pomodorosCount: 12,
+      tags: [
+        Tag(label: 'programming', color: Colors.purple.value),
+        Tag(label: 'javascript', color: Colors.grey.value),
+      ],
+      pomodorosCount: 34,
     ),
   ];
 
@@ -49,8 +60,41 @@ class GoalTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading: Text(goal.pomodorosCount.toString()),
+        leading: Text(
+          goal.pomodorosCount.toString(),
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
         title: Text(goal.goal.label),
+        subtitle: Wrap(
+          children: goal.tags.map((t) => TagListTile(t)).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class TagListTile extends StatelessWidget {
+  final Tag tag;
+
+  TagListTile(this.tag);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
+      margin: EdgeInsets.only(right: 2.0),
+      decoration: BoxDecoration(
+        color: Color(tag.color),
+        borderRadius: BorderRadius.circular(2.0),
+      ),
+      child: Text(
+        tag.label,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 10.0,
+        ),
       ),
     );
   }
