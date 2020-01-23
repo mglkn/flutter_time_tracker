46d95f2b0d3319d75b7ae61d70a207a6e33f925f
Vladimir <iammigalkin@yandex.com>
2020-01-23 10:38:09 +0300

    ui: add floatin action buttoon (create tag handler);

--- lib/src/ui/screens/tag_form_screen.dart
+++ lib/src/ui/screens/tag_form_screen.dart
@@ -4,16 +4,25 @@ import '../../utils/app_localization.dart';
 import '../widgets/forms/forms.dart';
 
 class TagFormScreen extends StatelessWidget {
+  _createTagHandler() {
+    print('create tag handler');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final title =
+        AppLocalizations.of(context).translate("createTag").toUpperCase();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          AppLocalizations.of(context).translate("createTag").toUpperCase(),
-        ),
+        title: Text(title),
         centerTitle: true,
       ),
       body: TagForm(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.done),
+        onPressed: _createTagHandler,
+      ),
     );
   }
 }
