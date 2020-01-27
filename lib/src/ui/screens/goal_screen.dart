f72d0d2f4eb02049a90e856a8604de736683a235
Vladimir <iammigalkin@yandex.com>
2020-01-27 13:43:32 +0300

    ui: add provider to goal_screen;

--- lib/src/ui/screens/goal_screen.dart
+++ lib/src/ui/screens/goal_screen.dart
@@ -1,7 +1,10 @@
 import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../data/dto.dart';
+import '../../data/db_repository.dart';
+import '../../store/goal_store.dart';
 
 class GoalScreen extends StatelessWidget {
   final GoalWithTagsAndPomodorosCount goal;
@@ -10,9 +13,28 @@ class GoalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Goal screen ${goal.label}'),
+    return Provider(
+      create: (_) => GoalStore(
+        db: DbDataRepository.db(),
+        goal: goal.goal,
+      ),
+      child: _GoalScreen(),
+    );
+  }
+}
+
+class _GoalScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (_, GoalStore store, __) => Scaffold(
+        appBar: AppBar(
+          title: Text(store.label),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Goal screen'),
+        ),
       ),
     );
   }
