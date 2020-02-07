0dcdb7c46890c7d632f277e13c034a2c1058ca38
Vladimir <iammigalkin@yandex.com>
2020-02-07 17:56:53 +0300

    main.dart: add commented debug paint statements;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+// import 'package:flutter/rendering.dart';
 
 import './src/app.dart';
 import './src/store/home_store.dart';
 import './src/data/db_repository.dart';
 
 void main() {
+  // debugPaintPointersEnabled = debugPaintBaselinesEnabled =
+  //     debugPaintLayerBordersEnabled = debugRepaintRainbowEnabled = true;
   runApp(
     Provider<HomeStore>(
       create: (_) => HomeStore(repo: DbDataRepository.db()),
