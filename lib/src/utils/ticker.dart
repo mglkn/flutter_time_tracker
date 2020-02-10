22ae2cc3e45f5044c9718734289b0213cc4e92ab
Vladimir <iammigalkin@yandex.com>
2020-02-10 19:15:52 +0300

    change ticker duration;

--- lib/src/utils/ticker.dart
+++ lib/src/utils/ticker.dart
@@ -1,6 +1,6 @@
 class Ticker {
   Stream<int> tick({int ticks}) {
     return Stream<int>.periodic(
-        Duration(milliseconds: 10), (x) => ticks - x - 1).take(ticks);
+        Duration(milliseconds: 1000), (x) => ticks - x - 1).take(ticks);
   }
 }
