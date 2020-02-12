56a765df9c7dba7bcf71bc553a7a040acfb64042
Vladimir <iammigalkin@yandex.com>
2020-02-12 17:31:00 +0300

    home_store tests finish;

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -4,6 +4,7 @@ import 'package:dartz/dartz.dart';
 
 import 'package:time_tracker/src/data/db_repository.dart';
 import 'package:time_tracker/src/data/dto.dart';
+import 'package:time_tracker/src/data/db.dart';
 
 class DBDataRepositoryMock extends Mock implements DbDataRepository {}
 
@@ -28,6 +29,38 @@ DBDataRepositoryMock getDBRepoWithGoalsAndTagsMocked({
   when(repo.getTags()).thenAnswer((_) {
     return Task(() {
       return Future.value(tags ?? <TagWithPomodorosCount>[]);
+    }).attempt().run();
+  });
+
+  return repo;
+}
+
+DBDataRepositoryMock setDBDataMockToErrorInGetGoals(
+    {DBDataRepositoryMock repo, String error}) {
+  when(repo.getGoals(false)).thenAnswer((_) {
+    return Task(() async {
+      throw Exception(error);
+    }).attempt().run();
+  });
+  return repo;
+}
+
+DBDataRepositoryMock setDBDataMockToErrorInGetTags(
+    {DBDataRepositoryMock repo, String error}) {
+  when(repo.getTags()).thenAnswer((_) {
+    return Task(() async {
+      throw Exception(error);
+    }).attempt().run();
+  });
+
+  return repo;
+}
+
+DBDataRepositoryMock setDbDataMovkToRemoveTag(
+    {DBDataRepositoryMock repo, Tag tag}) {
+  when(repo.removeTag(tag)).thenAnswer((_) {
+    return Task(() {
+      return Future.value(1);
     }).attempt().run();
   });
 
--- test/store/home_store_test.dart
+++ test/store/home_store_test.dart
@@ -5,10 +5,9 @@ import 'package:dartz/dartz.dart';
 import 'package:time_tracker/src/store/home_store.dart';
 import 'package:time_tracker/src/data/db_repository.dart';
 import 'package:time_tracker/src/data/dto.dart';
+import 'package:time_tracker/src/data/db.dart';
 
 import '../helpers/mocks.dart';
-
-class DBDataRepositoryMock extends Mock implements DbDataRepository {}
 
 void main() {
   test('init store should get goals and tags', () async {
@@ -28,6 +27,8 @@ void main() {
 
     expect(homeStore.goals.length, goals.length);
     expect(homeStore.tags.length, tags.length);
+    verify(repo.getGoals(false)).called(1);
+    verify(repo.getTags()).called(1);
   });
 
   test('initial goal done status should \'false\'', () {
@@ -44,5 +45,71 @@ void main() {
     homeStore.setGoalStatus(EGoalStatus.DONE);
 
     expect(homeStore.isGoalDone, true);
+    verify(repo.getGoals(false)).called(1);
+    verify(repo.getGoals(true)).called(1);
+  });
+
+  test('initial page index should equal 0', () {
+    final repo = getDBRepoWithGoalsAndTagsMocked();
+    final homeStore = HomeStore(repo: repo);
+
+    expect(homeStore.pageIndex, 0);
+  });
+
+  test('set page index should change it', () {
+    final repo = getDBRepoWithGoalsAndTagsMocked();
+    final homeStore = HomeStore(repo: repo);
+
+    homeStore.setPageIndex(1);
+
+    expect(homeStore.pageIndex, 1);
+  });
+
+  test('dbError should change if error occured into getGoals', () async {
+    final errorMessage = 'db error';
+    final repo = setDBDataMockToErrorInGetGoals(
+        repo: getDBRepoWithGoalsAndTagsMocked(), error: errorMessage);
+
+    final homeStore = HomeStore(repo: repo);
+
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    expect(homeStore.dbError, 'Exception: $errorMessage');
+  });
+
+  test('dbError should change if error occured into getTags', () async {
+    final errorMessage = 'db error';
+    final repo = setDBDataMockToErrorInGetTags(
+        repo: getDBRepoWithGoalsAndTagsMocked(), error: errorMessage);
+
+    final homeStore = HomeStore(repo: repo);
+
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    expect(homeStore.dbError, 'Exception: $errorMessage');
+  });
+
+  test('remove tag should call removeTag, getGoals and getTags', () async {
+    final tag = TagWithPomodorosCount(
+      tag: Tag(label: 'tag1', color: 0xFF000000),
+      pomodorosCount: 0,
+    );
+
+    final tags = [tag];
+
+    final repo = setDbDataMovkToRemoveTag(
+      repo: getDBRepoWithGoalsAndTagsMocked(tags: tags),
+      tag: tag.tag,
+    );
+
+    final homeStore = HomeStore(repo: repo);
+
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    await homeStore.deleteTag(tag);
+
+    verify(repo.removeTag(tag.tag)).called(1);
+    verify(repo.getGoals(false)).called(2);
+    verify(repo.getTags()).called(2);
   });
 }
