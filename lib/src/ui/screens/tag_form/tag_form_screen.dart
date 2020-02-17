e8b8a1e234cdbcb1dd597d396f25a2f212cd2b7e
Vladimir <iammigalkin@yandex.com>
2020-02-17 17:40:28 +0300

    fix: font styling in appbar tag form;

--- lib/src/ui/screens/tag_form/tag_form_screen.dart
+++ lib/src/ui/screens/tag_form/tag_form_screen.dart
@@ -73,7 +73,7 @@ class _AppBarTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       title,
-      style: Theme.of(context).textTheme.subtitle2.copyWith(
+      style: Theme.of(context).textTheme.headline6.copyWith(
             letterSpacing: 10.0,
           ),
     );
